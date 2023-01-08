c0609e7eb628f92f1f41f5eee2966cce849c2211
coderX3 <my_idea@live.cn>
2023-01-08 21:34:37 +0800

    useState

--- spa-simulate/src/App.js
+++ spa-simulate/src/App.js
@@ -8,24 +8,32 @@ export const myContext = React.createContext("defaultValue");
 myContext.displayName = "defined_context"
 
 export default class App extends Component {
-  
+
   render() {
     return (
-      <myContext.Provider value='上下文'>
+      <myContext.Provider value={{ name: 'lucy', age: 18 }}>
         <ul id='navigation'>
           <li>
-            <Link to={'/profile'} >profile</Link>
+            <Link to={'/profile'} > profile</Link>
           </li>
           <li>
             <Link to={'/dishes'}>dishes</Link>
           </li>
         </ul>
         <div id='display'>
-          <Routes>
-            {/* <Route path='/' element={<Dishes />} /> */}
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/dishes' element={<Dishes />} />
-          </Routes>
+          <myContext.Consumer>
+            {
+              (value) => {
+                return (
+                  <Routes>
+                    <Route path='/profile' element={<Profile account={value} />} />
+                    <Route path='/dishes' element={<Dishes />} />
+                  </Routes>
+                );
+              }
+            }
+          </myContext.Consumer>
+
         </div>
       </myContext.Provider>
     )
--- spa-simulate/src/pages/profile/Profile.jsx
+++ spa-simulate/src/pages/profile/Profile.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Profile() {
+export default function Profile(props) {
+  let [account, setAccount] = useState(props.account);
+  let [count, setCount] = useState(props.account.age);
+  function changeAccount() {
+    setAccount(
+      (account)=>{
+        return {name: 'allen',
+        age: 22
+        }
+      }
+    );
+  }
   return (
-    <div>user profile</div>
+    <div>
+      <div>{account.name}</div>
+      <div>{account.age}</div>
+      <div>{count}</div>
+      <button onClick={changeAccount}> 改变用户信息 </button>
+      <button onClick={() => { setCount(++count) }}> 点击计数 </button>
+    </div>
   )
 }

744c33131fc354c635a8e95b1286d77361ef067d
coderX3 <my_idea@live.cn>
2023-02-15 19:15:03 +0800

    登录

--- cooking-assistant/src/App.js
+++ cooking-assistant/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
-import { indexElements } from './routes/indexRoutes' 
+import React, {useState} from 'react'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+
 
 export default function App() {
-  const indexElement = useRoutes(indexElements);
+  let accountInfo = {};
+  let loginContext = React.createContext();
+  const [isLogin, setIsLogin] = useState(false);
+  
+  let getAccountInfo = (account)=>{
+    return accountInfo = account;
+  }
   return (
     <div>
-      {indexElement}
+      <loginContext.Provider>
+        {isLogin? <Home /> : <Login getAccountInfo={getAccountInfo}/>}
+      </loginContext.Provider>
     </div>
   )
 }
--- cooking-assistant/src/pages/login/Login.jsx
+++ cooking-assistant/src/pages/login/Login.jsx
@@ -2,10 +2,20 @@ import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 import './login.css'
 
+
 export default function Login() {
     const onFinish = (values) => {
         console.log('Received values of form: ', values);
     };
+    const login = ()=>{
+        fetch().then(
+            (response)=>{response.json()}
+        ).then(
+            (res)=>{
+            }
+        );
+    };
+
     return (
         <Form
             name="normal_login"
@@ -20,43 +30,45 @@ export default function Login() {
                 rules={[
                     {
                         required: true,
-                        message: 'Please input your Username!',
+                        message: '请输入您的用户名！',
                     },
                 ]}
             >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
             </Form.Item>
             <Form.Item
                 name="password"
                 rules={[
                     {
                         required: true,
-                        message: 'Please input your Password!',
+                        message: '请输入您的密码！',
                     },
                 ]}
             >
                 <Input
                     prefix={<LockOutlined className="site-form-item-icon" />}
                     type="password"
-                    placeholder="Password"
+                    placeholder="密码"
                 />
             </Form.Item>
             <Form.Item>
                 <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
+                    <Checkbox>记住我</Checkbox>
                 </Form.Item>
 
-                <a className="login-form-forgot" href="">
+                {/* <a className="login-form-forgot" href="">
                     Forgot password
-                </a>
+                </a> */}
             </Form.Item>
 
             <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
+                <Button type="primary" htmlType="submit" className="login-form-button" onClick={login}>
                     Log in
                 </Button>
-                Or <a href="">register now!</a>
+                {/* Or <a href="">register now!</a> */}
             </Form.Item>
         </Form>
     );
 }
+
+

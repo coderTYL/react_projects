8e910c8afffbacf0c8b75b413a5aec2c5640faaa
coderX3 <my_idea@live.cn>
2023-03-10 11:20:33 +0800

    quoteMachine

---
+++ quote-machine/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div id='quote-box'>
+      <p id='text'>
+        quote
+      </p>
+      <p id='author'>
+        author
+      </p>
+      <button id='new-quote'></button>
+      <a id='tweet-quote'></a>
+    </div>
+  )
+}
